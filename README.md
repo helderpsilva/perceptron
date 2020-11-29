c9dc0b617639aeea2e69130bd1625c5d8db85eec
Carla Lemos <carlaaireslemos@gmail.com>
2020-11-29 16:47:47 +0000

    Gerar amostra aleatória

--- README.md
+++ README.md
@@ -6,16 +6,26 @@
 
 <br/>
 
-##### Pós-Graduação em Data Science and Business Intelligence
-
 **Perceptron** também designado por neurônio artificial é uma função matemática de classificação binária (aprendizagem supervisionada). Análogo à unidade básica do sistema nervoso, esta recebe diversos imputs que são processados gerando um outpout único.
 
 ## Requisitos
 * Pynthon 3.8.5
 * Numpy 1.19.2
 
+## Preparar o ambiente
+Criar o ambiente
+
+`$ conda env create -f environment.yaml`
+
+Ativar o ambiente
+
+`$ conda activate perceptron` 
+
+Remover o ambiente
+
+`$ conda env remove -n perceptron`
+
 ## Créditos
 [Carla M. Lemos](https://github.com/CarlaMLemos) 
-
 [Hélder P. Silva ](https://github.com/helderpsilva)
 
--- perceptron.py
+++ perceptron.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import time
 
-# Todo:
+# To do:
 # Passo 0: Inicializar o vector de peso e o enviesamento com zeros (ou pequenos valores aleatórios).
 # Passo 1: Calcular uma combinação linear das características e pesos de entrada.
 # Passo 2: Aplicar a função sigmoide, que retorna valores binários
@@ -10,4 +10,19 @@ import time
 # Passo 4: Actualizar os pesos e o enviesamento.
 
 
+# Dataset
+# Gerar cluster aleatório normalmente distribuido  
+def sample_generator():
+    dados = {}
+    dados["x1"] = np.random.normal(loc=3.0, size=25)
+    dados["y1"] = np.random.normal(loc=2.0, size=25)
+    dados["x2"] = np.random.normal(loc=9.0, size=25)
+    dados["y2"] = np.random.normal(loc=7.0, size=25)
+    return dados
 
+dados = sample_generator()
+
+# visualizar dados
+plt.scatter(dados["x1"],dados["y1"],color='#023047', marker="o",s=25)
+plt.scatter(dados["x2"],dados["y2"],color='#fb8500', marker="o",s=25)
+plt.show()
