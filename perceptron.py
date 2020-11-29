6acebcc61d83d138bd47e3cb43c1ad3ce29d8eef
Helder Silva <silva.helder1996@gmail.com>
2020-11-29 01:34:39 +0000

    Criação do ambiente virtual - perceptron

---
+++ perceptron.py
@@ -0,0 +1,13 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import time
+
+# Todo:
+# Passo 0: Inicializar o vector de peso e o enviesamento com zeros (ou pequenos valores aleatórios).
+# Passo 1: Calcular uma combinação linear das características e pesos de entrada.
+# Passo 2: Aplicar a função sigmoide, que retorna valores binários
+# Passo 3: Calcular actualizações de peso usando as regras do perceptron.
+# Passo 4: Actualizar os pesos e o enviesamento.
+
+
+
