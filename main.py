277538a4a57bb82945cea0670e8df88cc73ab8ef
Helder Silva <helder.silva@polisport.com>
2021-12-22 19:07:17 +0000

    Updated project structure

--- main.py
+++ main.py
@@ -1,19 +1,25 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from perceptron import perceptron
+from src.perceptron import perceptron
 
 
 # Dataset
-# Gerar cluster aleatório normalmente distribuido  
+# Gerar cluster aleatório normalmente distribuido
+
 
 def sample_generator(sample_size=20):
-    x = (np.random.normal(loc=3.0, size=int(sample_size/2)).tolist()) + (np.random.normal(loc=9.0, size=int(sample_size/2)).tolist())
-    y = (np.random.normal(loc=2.0, size=int(sample_size/2)).tolist()) + (np.random.normal(loc=7.0, size=int(sample_size/2)).tolist())
-    label = [1 if i > int(sample_size/2-1)  else 0 for i in range(sample_size)]
+    x = (np.random.normal(loc=3.0, size=int(sample_size / 2)).tolist()) + (
+        np.random.normal(loc=9.0, size=int(sample_size / 2)).tolist()
+    )
+    y = (np.random.normal(loc=2.0, size=int(sample_size / 2)).tolist()) + (
+        np.random.normal(loc=7.0, size=int(sample_size / 2)).tolist()
+    )
+    label = [1 if i > int(sample_size / 2 - 1) else 0 for i in range(sample_size)]
     return x, y, label
 
+
 sample = 100
-x,y,label = sample_generator(sample)
+x, y, label = sample_generator(sample)
 
 data = np.column_stack((x, y))
 data_label = np.array(label)
@@ -24,9 +30,10 @@ data_label = np.array(label)
 # 2. Minima alteração do erro
 # 3. Probabilidade do utilizador escolher uma ou outra classe
 
+
 def user_imputs():
-# O programa corre o máximo número de interações ou o menor erro, consoante o que ocorrer primeiro
-    print ("\n 1.Número de iterações (default = 100)") 
+    # O programa corre o máximo número de interações ou o menor erro, consoante o que ocorrer primeiro
+    print("\n 1.Número de iterações (default = 100)")
     n_iterations = input()
     if n_iterations == "":
         no_compliance = False
@@ -41,8 +48,8 @@ def user_imputs():
         except ValueError:
             print("Por favor, indique um número inteiro")
             n_iterations = input()
-    
-    print ("2. Mínima alteração de erro (default = 0)") 
+
+    print("2. Mínima alteração de erro (default = 0)")
     min_error = input()
     if min_error == "":
         no_compliance = False
@@ -57,8 +64,10 @@ def user_imputs():
         except ValueError:
             print("Por favor, indique um número")
             min_error = input()
-    
-    print ("3. Probabilidade a partir da qual o algoritmo escolhe uma ou outra classe (default = 0.5)")
+
+    print(
+        "3. Probabilidade a partir da qual o algoritmo escolhe uma ou outra classe (default = 0.5)"
+    )
     prob = input()
     if prob == "":
         no_compliance = False
@@ -73,20 +82,21 @@ def user_imputs():
             else:
                 no_compliance = False
                 break
-            break 
+            break
         except ValueError:
             print("Por favor, indique um número entre 0 e 1")
             prob = input()
 
     return n_iterations, min_error, prob
 
+
 n_iterations, min_error, prob = user_imputs()
 
 # Inicializar uma nova instância do perceptron com os dados obtidos do utilizador.
-p = perceptron(n_iterations,0.01, min_error)
+p = perceptron(n_iterations, 0.01, min_error)
 
 # Normalização dos dados.
-standard_data = p.scale(data,0,1)
+standard_data = p.scale(data, 0, 1)
 
 # Treino do modelo.
 fitted = p.fit(standard_data, data_label)
@@ -96,11 +106,12 @@ predicted = p.predict(standard_data)
 predicted_adjusted = [1 if predicted[i] > prob else 0 for i in range(sample)]
 
 # Gerar visualização gráfica da previsão obtida.
-x = np.linspace(min(standard_data[:,0]), max(standard_data[:,0]), num=50)
-m = (-(p.bias / p.weights[1])/(p.bias / p.weights[0]))*x + (- p.bias / p.weights[1])
+x = np.linspace(min(standard_data[:, 0]), max(standard_data[:, 0]), num=50)
+m = (-(p.bias / p.weights[1]) / (p.bias / p.weights[0])) * x + (-p.bias / p.weights[1])
 
 
-plt.scatter(standard_data[:,0], standard_data[:,1],c=predicted_adjusted, marker="o",s=25)
-plt.plot(x,m)
+plt.scatter(
+    standard_data[:, 0], standard_data[:, 1], c=predicted_adjusted, marker="o", s=25
+)
+plt.plot(x, m)
 plt.show()
-
--- src/perceptron.py
+++ src/perceptron.py
@@ -1,4 +1,4 @@
-#Importação das Livrarias
+# Importação das Livrarias
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -9,7 +9,8 @@ import matplotlib.pyplot as plt
 # Passo 3: Calcular actualizações de peso usando as regras do perceptron.
 # Passo 4: Actualizar os pesos e o enviesamento.
 
-class perceptron():
+
+class perceptron:
     """
     Perceptron: Nerónio artificial para classificação binária
     ...
@@ -21,17 +22,17 @@ class perceptron():
 
     """
 
-    def __init__ (self, n_iterations = 100, learning_rate = 0.01, min_error = 0):
+    def __init__(self, n_iterations=100, learning_rate=0.01, min_error=0):
         # Iniciar pesos e enviesamento.
         self.iterations = n_iterations
         self.learning = learning_rate
         self.activation_function = self.sigmoid
         self.weights = None
         self.min_error = min_error
-        #self.prob = 0.5
+        # self.prob = 0.5
 
-    def fit(self, X, y):   
-        # Função de Treino    
+    def fit(self, X, y):
+        # Função de Treino
         n_samples, n_features = X.shape
         self.weights = np.zeros(n_features)
         self.bias = 0.5
@@ -43,25 +44,25 @@ class perceptron():
         error_dif = 0
 
         for _ in range(self.iterations):
-            for index, xi in  enumerate(X):
+            for index, xi in enumerate(X):
                 predicted_value = self.predict(xi)
-                update = self.learning * (y_real[index] - predicted_value) 
+                update = self.learning * (y_real[index] - predicted_value)
 
-                self.weights += update * xi 
+                self.weights += update * xi
                 self.bias += update
 
                 error = y_real[index] - predicted_value
-                sum_error += error**2
+                sum_error += error ** 2
 
             error_list.append(sum_error)
             try:
-                error_dif = abs(error_list[-2]-error_list[-1])
+                error_dif = abs(error_list[-2] - error_list[-1])
             except:
                 error_dif = error_list[0]
                 pass
             count += 1
-                
-            print(f'Iteração n. {count} com erro de {error_dif}')
+
+            print(f"Iteração n. {count} com erro de {error_dif}")
             if error_dif <= self.min_error:
                 break
 
@@ -70,15 +71,15 @@ class perceptron():
         activation = np.dot(X, self.weights) + self.bias
         y_predicted = self.activation_function(activation)
         # y_predicted = np.where(self.activation_function(activation)>self.prob,1,0)
-        return y_predicted       
+        return y_predicted
 
     def sigmoid(self, X):
         # Função de Ativação (0-1)
-         return 1/(1+np.exp(-X))
+        return 1 / (1 + np.exp(-X))
 
     def scale(self, X, x_min, x_max):
         # Função de normalização dos dados.
-        nom = (X-X.min(axis=0))*(x_max-x_min)
+        nom = (X - X.min(axis=0)) * (x_max - x_min)
         denom = X.max(axis=0) - X.min(axis=0)
-        denom[denom==0] = 1
-        return x_min + nom/denom
+        denom[denom == 0] = 1
+        return x_min + nom / denom
